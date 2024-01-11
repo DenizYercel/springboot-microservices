ef1cb1d39b073f79956fbdc22574918c315d1001
Deniz <denizyrcl@gmail.com>
2024-01-11 11:17:15 +0300

    Service Registry,Spring Cloud Netflix Eureka,LoadBalancer için örnek yapıldı.

--- employee-service/src/main/java/com/denizyercel/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/denizyercel/employeeservice/EmployeeServiceApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableFeignClients
 @ImportAutoConfiguration({FeignAutoConfiguration.class})
+@EnableEurekaClient
 public class EmployeeServiceApplication {
 
 	/*	@Bean
