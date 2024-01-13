0de2e3ce1de2142da40242ab4959d3315bad9655
Deniz <denizyrcl@gmail.com>
2024-01-13 17:09:40 +0300

    Circuit Breaker ve  Retry ile Resilience4j kullanılan bir örnek için geliştirme yapıldı.

--- employee-service/src/main/java/com/denizyercel/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/denizyercel/employeeservice/EmployeeServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -16,15 +17,16 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class EmployeeServiceApplication {
 
 	/*	@Bean
+	    @LoadBalanced
 	public RestTemplate restTemplate(){
 		return new RestTemplate();
 	}
 	*/
 
-	/*@Bean
+	@Bean
 	public WebClient webClient(){
 		return WebClient.builder().build();
-	} */
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeServiceApplication.class, args);
--- employee-service/src/main/java/com/denizyercel/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/denizyercel/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -8,7 +8,11 @@ import com.denizyercel.employeeservice.mapper.AutoEmployeeMapper;
 import com.denizyercel.employeeservice.repository.EmployeeRepository;
 import com.denizyercel.employeeservice.service.APIClient;
 import com.denizyercel.employeeservice.service.EmployeeService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -20,8 +24,10 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     private EmployeeRepository employeeRepository;
     // private RestTemplate restTemplate;
-    //private WebClient webClient;
-    private APIClient apiClient;
+    private WebClient webClient;
+    //private APIClient apiClient;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeServiceImpl.class);
 
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
@@ -31,8 +37,12 @@ public class EmployeeServiceImpl implements EmployeeService {
         return  savedEmployeeDto;
     }
 
+    //@CircuitBreaker(name = "${spring.application.name}",fallbackMethod = "getDefaultDepartment" )
+    @Retry(name = "${spring.application.name}",fallbackMethod = "getDefaultDepartment")
     @Override
     public APIResponseDto getEmployeeById(Long employeeId) {
+
+        LOGGER.info("inside getEmployeeId() method");
         Employee employee = employeeRepository.findById(employeeId).get();
 
 //        ResponseEntity<DepartmentDto> responseEntity =  restTemplate.getForEntity("http://localhost:8080/api/departments/" + employee.getDepartmentCode(),
@@ -40,18 +50,34 @@ public class EmployeeServiceImpl implements EmployeeService {
 //
 //        DepartmentDto departmentDto = responseEntity.getBody();
 
-//        DepartmentDto departmentDto = webClient.get()
-//                .uri("http://localhost:8080/api/departments/" + employee.getDepartmentCode())
-//                .retrieve()
-//                .bodyToMono(DepartmentDto.class)
-//                .block();
+        DepartmentDto departmentDto = webClient.get()
+                .uri("http://localhost:8080/api/departments/" + employee.getDepartmentCode())
+                .retrieve()
+                .bodyToMono(DepartmentDto.class)
+                .block();
 
-        DepartmentDto departmentDto = apiClient.getDepartment(employee.getDepartmentCode());
+        //DepartmentDto departmentDto = apiClient.getDepartment(employee.getDepartmentCode());
 
         APIResponseDto apiResponseDto = new APIResponseDto();
 
         apiResponseDto.setEmployeeDto(AutoEmployeeMapper.MAPPER.maptoEmployeeDto(employee));
         apiResponseDto.setDepartmentDto(departmentDto);
+        return apiResponseDto;
+    }
+
+    public APIResponseDto getDefaultDepartment(Long employeeId,Exception exception){
+        LOGGER.info("inside getDefaultDepartment() method");
+        Employee employee = employeeRepository.findById(employeeId).get();
+
+        DepartmentDto departmentDto = new DepartmentDto();
+        departmentDto.setDepartmentName("R&D Department");
+        departmentDto.setDepartmentDescription("RDDB1");
+        departmentDto.setDepartmentCode("Research and Development Department");
+
+        APIResponseDto apiResponseDto = new APIResponseDto();
+        apiResponseDto.setEmployeeDto(AutoEmployeeMapper.MAPPER.maptoEmployeeDto(employee));
+        apiResponseDto.setDepartmentDto(departmentDto);
+
         return apiResponseDto;
     }
 }
